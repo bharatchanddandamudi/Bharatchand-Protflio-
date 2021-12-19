b298a105dc166c9da5ed13d0973969abc62c2648
Bharatchand Dandamudi <bharatchand.tree@gmail.com>
2021-12-19 21:03:03 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -83,20 +83,20 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
-        'USER':'postgres',
-        'PASSWORD': os.environ.get('SECRET_PASSWORD'),
-        'HOST': 'localhost',
+        'NAME': 'dbep760tpds4tc',
+        'USER':'royifaapcqkyya',
+        'PASSWORD': '5ea8377dfbf00b2cf347e7029992c72e39bec3d03d96a4afd631157ca8f5b109',
+        'HOST': 'ec2-34-226-178-146.compute-1.amazonaws.com',
         'PORT':'5432',
     }	
 }
-DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-  }	
-}
-DATABASE_URL = os.environ.get('DATABASE_URL')
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#   }	
+# }
+# DATABASE_URL = os.environ.get('DATABASE_URL')
 
 
 # Password validation
