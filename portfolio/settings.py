cbf1a2e8bd82b3d4ec291f7b81f1990ba67b3200
Bharatchand Dandamudi <bharatchand.tree@gmail.com>
2021-12-22 23:43:40 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -144,7 +144,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'portfolio/static/'),
+    os.path.join(BASE_DIR, 'jobs/static/'),
 )
 
 
