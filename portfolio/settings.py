1a3143b8f5451f738facbb33012d0e5cc859ebd7
Bharatchand Dandamudi <bharatchand.tree@gmail.com>
2021-12-22 21:14:06 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATIC_ROOT = (BASE_DIR, 'staticfilies')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+ STATIC_ROOT = (BASE_DIR, 'staticfilies')
 
 
 MEDIA_ROOT = BASE_DIR
