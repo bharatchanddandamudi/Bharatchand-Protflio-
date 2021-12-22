b0e0f086bc22f5e3836b2b54dcb4323bbfd684bb
Bharatchand Dandamudi <bharatchand.tree@gmail.com>
2021-12-22 22:38:58 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,8 +137,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#  STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#  STATIC_ROOT = (BASE_DIR, 'staticfilies')
 
 
 MEDIA_ROOT = BASE_DIR
